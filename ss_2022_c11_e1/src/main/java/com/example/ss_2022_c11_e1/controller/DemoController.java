fe8f5308832ba38326cc5a88178900374f1b783f
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-01-02 20:07:09 -0300

    Added lesson 11 project

---
+++ ss_2022_c11_e1/src/main/java/com/example/ss_2022_c11_e1/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.ss_2022_c11_e1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+   @GetMapping
+   public String hello() {
+       return "Hello, world!";
+   }
+}
